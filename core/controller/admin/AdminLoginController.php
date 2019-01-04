d2921ad065756e01ad356fef1382be2b565bc309
tdc <tdc@wizzmedia.fr>
2019-01-04 22:04:18 +0100

    Add admin reset password page

--- core/controller/admin/AdminLoginController.php
+++ core/controller/admin/AdminLoginController.php
@@ -17,6 +17,108 @@ class AdminLoginController extends  AdminController
         echo $this->viewManager->render("pages/login.html");
 
     }
+    public function resetpassword()
+    {
+        $context = Context::getContext();
+
+        $link = Tools::getValue('link');
+        if (!$link) {
+            if (Tools::isSubmit("sendEmail")) {
+                $email = Tools::getValue("email");
+                $id_administrator = Administrator::emailExist($email);
+
+                if ($id_administrator) {
+                    $admin = new Administrator($id_administrator);
+                    $link = $admin->genereateResetPasswordLink();
+                    Mail::send($admin->mail, 'Your password reset link', 'resetpassword_admin', ['firstname' => $admin->getFirstname(),
+                        'lastname' => $admin->getLastname(),
+                        'link' => $context->getBaseurl() . _ADMIN_URI_ . '/resetpassword?link=' . $link
+                    ]);
+                    $this->viewManager->initVariable(
+
+                        array('succes' => Tools::translate("reset link send by email"),
+
+                        ));
+                } else {
+                    $this->viewManager->initVariable(
+
+                        array('error' => Tools::translate("Not account found"),
+
+                        ));
+                }
+
+
+            }
+            echo $this->viewManager->render("pages/renewpasswordform.html");
+        }else
+        {
+
+            if (!Tools::isSubmit("resetpassword")) {
+
+                $id_administrator = Administrator::getAdministratorIdFromLink($link);
+                if (!$id_administrator)
+                {
+                    $this->viewManager->initVariable(
+
+                        array('error' => Tools::translate("Invalid Link"),
+
+                        ));
+                }else {
+                    $this->viewManager->initVariable(
+
+                        array('link' => Tools::translate($link),
+
+                        ));
+                }
+                echo $this->viewManager->render("pages/renewpasswordform.html");
+            }else
+            {
+                $id_administrator = Administrator::getAdministratorIdFromLink($link);
+                if (!$id_administrator)
+                {
+                    $this->viewManager->initVariable(
+
+                        array('error' => Tools::translate("Invalid Link"),
+
+                        ));
+                    echo $this->viewManager->render("pages/renewpasswordform.html");
+                }else {
+
+                    $error= '';
+                    $admin = new Administrator($id_administrator);
+                    $password = Tools::getValue("password");
+                    $password2 = Tools::getValue("password2");
+                    if ($password != $password2) {
+                        $error = 'password verification faild';
+                    }
+                    if ($password) {
+                        if (!Validate::isPasswd($password)) {
+                            $error = 'your password not success security ';
+
+                        }
+                    }
+                    if ($error) {
+                        $this->viewManager->initVariable(
+
+                            array('error' => Tools::translate($error),'link'=>$link
+
+                            ));
+                        echo $this->viewManager->render("pages/renewpasswordform.html");
+                    } else {
+
+                        $admin->newpassword = $password;
+                        $admin->update();
+                    }
+                    Tools::redirect(_BASE_URL_.'/' . _ADMIN_URI_  . '/login');
+
+
+
+
+                }
+            }
+        }
+
+    }
 
     public function logout()
     {
